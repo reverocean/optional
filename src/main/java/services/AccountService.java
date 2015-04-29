1ea43ec9cec23e0cac3b08ea8ca4049d1c59acb0
Haiyang He <reverocean@gmail.com>
2015-04-29 17:15:31 +0800

    implemation the getCityName method to make all test pass

--- src/main/java/services/AccountService.java
+++ src/main/java/services/AccountService.java
@@ -2,13 +2,24 @@ package services;
 
 import repositories.AccountRepository;
 import vo.Account;
+import vo.Address;
+import vo.City;
 
 public class AccountService {
     private AccountRepository accountRepository;
 
     public String getCityName(int accountId) {
         Account account = accountRepository.findAccount(accountId);
-        return account.getAddress().getCity().getName();
+        if (account != null) {
+            Address address = account.getAddress();
+            if (address != null) {
+                City city = address.getCity();
+                if (city != null) {
+                    return city.getName();
+                }
+            }
+        }
+        return null;
     }
 
 
